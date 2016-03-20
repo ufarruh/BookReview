a0578a329761fe724acaa2f70a90101367fcb88f
Farruh <ufarruh@gmail.com>
2016-03-20 00:12:21 +0000

    Change dvelopment.rb to true

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319224505) do
+ActiveRecord::Schema.define(version: 20160320000725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20160319224505) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
